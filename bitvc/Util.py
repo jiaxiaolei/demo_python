1929d49ca857404098066eb6f971c79363decb6d
jiaxiaolei <jiaxiaolei@cicaero.com>
2016-07-20 10:33:46 +0800

    Feature:/ BugId:/ Author:/ Jia xiaolei Description: 1. add key

--- bitvc/Util.py
+++ bitvc/Util.py
@@ -6,9 +6,11 @@ import hmac
 import base64
 
 #在此输入您的Key
-ACCESS_KEY=""
-SECRET_KEY=""
+# ACCESS_KEY=""
+# SECRET_KEY=""
 
+ACCESS_KEY="a7b18bd1-d58459b8-d1faba30-1519b"
+SECRET_KEY="6641d003-a7e72e01-c91c8711-3c7ba"
 
 def signature(params):
     params = sorted(params.iteritems(), key=lambda d:d[0], reverse=False)
--- bityes/Util.py
+++ bityes/Util.py
@@ -6,8 +6,12 @@ import hmac
 import base64
 
 #在此输入您的Key
-ACCESS_KEY=""
-SECRET_KEY=""
+# ACCESS_KEY=""
+# SECRET_KEY=""
+
+ACCESS_KEY="a7b18bd1-d58459b8-d1faba30-1519b"
+SECRET_KEY="6641d003-a7e72e01-c91c8711-3c7ba"
+
 
 BITYES_SERVICE_API="https://api.bityes.com/apiv2"
 
